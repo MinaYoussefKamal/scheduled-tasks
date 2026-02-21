15945f479ec3d5c6b7745f2fcaac5304b6a9c5b6
MinaYoussefKamal <minayoussefkamal@gmail.com>
2026-02-21 17:47:53 +0200

    Update main.py
    rain checker with WhatsApp messaging

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+MY_LAT=30.101847
+MY_LONG=31.262802
+OWM_api_key=os.environ.get("OWM_API_KEY")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+account_sid = "ACbbb87e044b0db1bbeea97ab09bc5b51a"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+
+
+client = Client(account_sid, auth_token)
+
+
+
+
+OWM_parameters = {
+    "lat": -1.683500,
+    "lon": 29.235600,
+    "appid": OWM_api_key,
+    "units": "metric",
+    "cnt": 4
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=OWM_parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+
+
+
+is_raining = False
+
+
+
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) <700:
+        print("Bring an Umbrella")
+        is_raining = True
+        break
+
+if is_raining:
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an Umbrella ☔",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+201205978443",
+    )
+    print(message.status)
+
